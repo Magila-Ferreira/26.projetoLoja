3b37b6cc1d96eaca4e66118a2e195dc51e7ccd12
Magila-Ferreira <magilamorganasf@hotmail.com>
2023-02-04 16:48:21 -0300

    Alterações lista_Cliente

--- js/pedido.js
+++ js/pedido.js
@@ -1,16 +1,36 @@
-/* var pedido = document.querySelector("#cadastro");
+var pedido = document.querySelector("#cadastro");
 var cpf = pedido.querySelector("input");
 
+window.onload = function () {
+    let lista_cliente = cliente();
+    salvarClientes(lista_cliente);
+}
 function cliente() {
-    
+    let lista_Cliente = [];
+
+    let cliente = {
+        cpf: "353.938.568-14",
+        nome: "Andréa Cristina da Silva",
+        email: "andera.c.s5@gmail.com",
+        cep: "13.902-402",
+        logradouro: "Rua Ângelo Bastos",
+        numero: "26",
+        bairro: "Jd. Santa Júlia",
+        cidade: "Amparo",
+        estado: "São Paulo"
+    };
+    lista_Cliente.push(cliente);
+    console.log(cliente.cpf);
+    return lista_Cliente;
+}
+function salvarClientes(lista_cliente) {
+    if (window.localStorage) {
+        if (window.localStorage.getItem("cpf") !== cpf.value) {
+            // Guardar um vetor no localStorage
+            gravaDados("clientes", lista_cliente);
+        }
+    } else {
+        alert("Local Storage indisponível");
+    }
 }
 
-
-if (window.localStorage) {
-    if (window.localStorage.getItem("cpf") !== cpf.value) {       
-        // Guardar um vetor no localStorage
-        gravaDados("cpf", lista_Cliente);
-    }
-} else {
-    alert("Local Storage indisponível");
-}  */
